ad63fdd622314c679f3b63f5c0aa68ba19f11a1a
Wylliane Cavalcante Spinellis <wyllianecavalcante@gmail.com>
2026-03-28 02:29:14 -0300

    Add area calculation for a rectangular plot of land

---
+++ area_terreno.py
@@ -0,0 +1,9 @@
+# Entrada de dados
+largura = float(input("Largura: "))
+comprimento = float(input("Comprimento: "))
+
+# Cálculo da área
+area = largura * comprimento
+
+# Saída
+print(f"Área do terreno:",area, "m²")
